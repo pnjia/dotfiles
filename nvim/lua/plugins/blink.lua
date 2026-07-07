6f495bbcd5a845ba96e781b41748912c19ad623d
pnjia <panjiangkasaputra67@gmail.com>
2026-07-08 05:57:04 +0700

    Menambahkan konfigurasi baru

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,15 +1,16 @@
 return {
   {
     "saghen/blink.cmp",
-    opts = {
-      completion = {
-        menu = {
-          auto_show = false,
-        },
-        ghost_text = {
-          enabled = false,
-        },
-      },
-    },
+    opts = function(_, opts)
+      opts.completion = opts.completion or {}
+      opts.completion.menu = vim.tbl_deep_extend("force", opts.completion.menu or {}, {
+        auto_show = false,
+      })
+      opts.completion.ghost_text = vim.tbl_deep_extend("force", opts.completion.ghost_text or {}, {
+        enabled = false,
+      })
+      opts.keymap = opts.keymap or {}
+      opts.keymap["<C-k>"] = { "scroll_documentation_up", "fallback" }
+    end,
   },
 }
--- nvim/lua/plugins/custom.lua
+++ nvim/lua/plugins/custom.lua
@@ -79,29 +79,12 @@ return {
       },
     },
     opts = {
-      open_for_directories = false,
+      open_for_directories = true,
       yazi_cmd = "/usr/bin/yazi",
     },
     init = function()
-      -- Disable netrw early
       vim.g.loaded_netrwPlugin = 1
       vim.g.loaded_netrw = 1
-      -- Open yazi when nvim starts with a directory arg
-      vim.api.nvim_create_autocmd("VimEnter", {
-        group = vim.api.nvim_create_augroup("Yazi_start_with_dir", { clear = true }),
-        desc = "Open yazi for directory arguments",
-        once = true,
-        callback = function()
-          local args = vim.fn.argv()
-          if #args > 0 and vim.fn.isdirectory(args[1]) == 1 then
-            local bufnr = vim.api.nvim_get_current_buf()
-            vim.schedule(function()
-              pcall(vim.api.nvim_buf_delete, bufnr, { force = true })
-              require("yazi").yazi(nil, args[1])
-            end)
-          end
-        end,
-      })
     end,
   },
 
